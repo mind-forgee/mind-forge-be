c6105bbe549363b0cd7772f92bf41a7b0153e744
jevvonn <darmawanb985@gmail.com>
2025-08-16 04:50:46 +0700

    feat: add admin verification to topic creation route

--- src/features/topic/topic.routes.ts
+++ src/features/topic/topic.routes.ts
@@ -3,9 +3,16 @@ import { createTopic } from "./topic.controller";
 import { validate } from "../../http/validate";
 import { createTopicSchema } from "./topic.schema";
 import { verifyToken } from "../../middleware/verifyToken";
+import { isAdmin } from "../../middleware/isAdmin";
 
 const router = express.Router();
 
-router.post("/", verifyToken, validate(createTopicSchema, "body"), createTopic);
+router.post(
+  "/",
+  verifyToken,
+  isAdmin,
+  validate(createTopicSchema, "body"),
+  createTopic,
+);
 
 export default router;
