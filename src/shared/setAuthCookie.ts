40766879c875b3edc627d7131bb6b6eaca7f6f6b
hadienzz <hadinpramiadi@gmail.com>
2025-08-22 09:12:18 +0700

    save local change before pulling

--- src/shared/setAuthCookie.ts
+++ src/shared/setAuthCookie.ts
@@ -3,7 +3,7 @@ import { Response } from "express";
 export const setAuthCookie = (token: string, res: Response) => {
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: false,
+    sameSite: "lax",
   });
 };
